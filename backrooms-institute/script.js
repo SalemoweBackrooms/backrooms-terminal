9ff3ca80b0d1a70998069321ffc719d38c57b813
SalemoweBackrooms <nickolas.lefent@gmail.com>
2026-06-28 15:53:00 +0200

    Update script.js

--- backrooms-institute/script.js
+++ backrooms-institute/script.js
@@ -1,1 +1,27 @@
-alert("JS działa");
+window.onload = function() {
+  setTimeout(() => {
+    document.getElementById("boot").classList.add("hidden");
+    document.getElementById("app").classList.remove("hidden");
+
+    document.getElementById("nadzorca").innerText =
+      "SYSTEM AKTYWNY. CZEKAM NA REKRUTACJĘ.";
+  }, 1500);
+};
+
+function startForm() {
+  document.getElementById("form").classList.remove("hidden");
+}
+
+function submitForm() {
+  let name = document.getElementById("name").value;
+  let age = document.getElementById("age").value;
+
+  document.getElementById("form").classList.add("hidden");
+
+  document.getElementById("result").classList.remove("hidden");
+  document.getElementById("result").innerHTML =
+    "REKRUT ZAREJESTROWANY<br><br>" +
+    "IMIĘ: " + name + "<br>" +
+    "WIEK: " + age + "<br><br>" +
+    "STATUS: W TRAKCIE ANALIZY";
+}
